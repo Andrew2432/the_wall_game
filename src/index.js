fda5c5a3de1492d7b7ac79a5a068439f6577b335
Andrew Joel <drew.devops@gmail.com>
2020-02-02 00:06:15 +0530

    basics

--- src/index.js
+++ src/index.js
@@ -2,3 +2,10 @@ import "./styles.css";
 
 const canvas = document.querySelector("#gameScreen");
 const context = canvas.getContext("2d");
+
+// Clear screen each time
+context.clearRect(0, 0, 800, 600);
+
+// Draw a simple rect
+context.fillStyle = "#0f0";
+context.fillRect(40, 10, 10, 10);
