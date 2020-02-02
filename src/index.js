b780bb0a538bad621e90312397e408b0572f5372
Andrew Joel <drew.devops@gmail.com>
2020-02-02 15:52:58 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import "./styles.css";
+import Paddle from "../src/paddle";
 
 const canvas = document.querySelector("#gameScreen");
 const context = canvas.getContext("2d");
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
 
 // Clear screen each time
 context.clearRect(0, 0, 800, 600);
@@ -9,3 +12,9 @@ context.clearRect(0, 0, 800, 600);
 // Draw a simple rect
 context.fillStyle = "#0f0";
 context.fillRect(40, 10, 10, 10);
+
+// Use paddle
+context.clearRect(0, 0, 800, 600);
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+context.fillStyle = "#000";
+paddle.draw(context);
